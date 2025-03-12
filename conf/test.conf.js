2816b691ac2fd8d91115cd82295948aee53d0fdc
Aman Deswal <adeswal@Amans-MacBook-Air.local>
2025-03-11 17:49:32 -0700

    my test suite 3

--- conf/test.conf.js
+++ conf/test.conf.js
@@ -1,6 +1,6 @@
 export const config = {
-  user: process.env.BROWSERSTACK_USERNAME,
-  key: process.env.BROWSERSTACK_ACCESS_KEY,
+  username: process.env.BROWSERSTACK_USERNAME,
+  accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
   hostname: 'hub.browserstack.com',
   services: [
     [
@@ -44,6 +44,8 @@ export const config = {
       networkLogs: "true",
       consoleLogs: "info",
       networkLogs: true,
+      userName: username,
+      accessKey : accessKey,
     }
   },
   mochaOpts: {
